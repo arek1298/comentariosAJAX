27dffe49dadeabb2d16f79d44abea3f1689e0447
ArekJavi <75266504+arek1298@users.noreply.github.com>
2024-02-01 23:00:13 -0600

    Actualizar procesar_comentario.php

--- procesar_comentario.php
+++ procesar_comentario.php
@@ -1,10 +1,10 @@
 <?php 
 include 'db.php';
-// Recibir datos del formulario
+
 $nombre = $_POST['nombre'];
 $comentario = $_POST['comentario'];
 
-// Insertar comentario en la base de datos
+
 $sql = "INSERT INTO comentarios (nombre, comentario) VALUES ('$nombre', '$comentario')";
 
 if ($conn->query($sql) === TRUE) {
@@ -13,6 +13,6 @@ if ($conn->query($sql) === TRUE) {
     echo "Error al enviar el comentario: " . $conn->error;
 }
 
-// Cerrar la conexión
+
 $conn->close();
 ?>
